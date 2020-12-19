fda30fa86fa496c03c57ad87dbc4702f7ed5e8fa
ModelMedia <75977597+ModelMedia@users.noreply.github.com>
2020-12-19 14:25:09 +0530

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :nadia, token: (System.get_env("BOT_TOKEN") || "")
+config :nadia, token: (System.get_env("1276418012:AAGoeonqH3rDe2DucSeFzdJjN-1Iyu-4FTs") || "")
 
 config :whos_in_bot, ecto_repos: [WhosInBot.Repo]
 
